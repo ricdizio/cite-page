bc3a8518f17914f977619377770c44e34b598ee3
Ricardo Di Zio <11-11274@usb.ve>
2017-04-27 11:20:46 -0400

    js corregido

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
  
   $("#owl-example").owlCarousel({
-	items : 5, //10 items above 1000px browser width
+	items : 4, //10 items above 1000px browser width
 	itemsDesktop : [1600,4], //5 items between 1000px and 901px
 	itemsDesktopSmall : [900,4], // betweem 900px and 601px
 	itemsTablet: [600,2], //2 items between 600 and 0
