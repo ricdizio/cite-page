b81203a0116b5f3679120bfb6bf3568b5245fe8e
Ricardo Di Zio <11-11274@usb.ve>
2017-04-30 17:10:39 -0400

    cambios

--- js/galeria.js
+++ js/galeria.js
@@ -1,17 +1,7 @@
-//galeria
-$galeria = $('.galeria');
-$galeria.find('.item').click(function (event) {
-loadDatos($(this).find('img').attr('src'), $(this).find('.descripcion').html());
-    $modal_galeria.show()
-});
+ var cuadro = document.getElementById("longPicture");
 
-//modal galeria
-$modal_galeria = $('.modal-galeria');
-$modal_galeria.find('#btn-cerrar').click(function (event) {
-    $modal_galeria.hide('slow');
-});
-
-var loadDatos = function (srcImg, descripcion) {
-    $modal_galeria.find('img').attr('src',srcImg);
-    $modal_galeria.find('#descripcion').html(descripcion);
+function cambiarFoto(obj){
+    var imgClick = obj ;
+    cuadro.src = imgClick.getAttribute('src').toString();
+    //alert("click: " + imgClick.getAttribute('src') +" cuadro : " + cuadro.src);
 }
